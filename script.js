4234b9b713765ad0a1540c8c68c39fb6fe983779
Dwi Bagaskara <dwibagaskara55@gmail.com>
2023-03-08 13:54:53 +0700

    responsive footer nav

---
+++ script.js
@@ -0,0 +1,18 @@
+const hamburger = document.getElementsByClassName('hamburger')[0];
+const navUl = document.getElementsByClassName('ul')[0];
+
+hamburger.addEventListener('click', () => {
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        // kode yang ingin dijalankan jika ukuran layar kurang dari atau sama dengan 768px
+        if(navUl.style.opacity == '1'){
+            navUl.style.opacity = '0';
+            navUl.style.transform = 'translateY(30px)'
+        } else {
+            navUl.style.opacity = '1';
+            navUl.style.transform = 'translateY(0)'
+        }
+    } else {
+        // kode yang ingin dijalankan jika ukuran layar lebih besar dari 768px
+        navUl.style.opacity = '1';
+      }
+})
